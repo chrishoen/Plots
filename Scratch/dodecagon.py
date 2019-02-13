409671cdda4b6ab8ce2d002d07ffc57f8e6f7e8c
chrishoen <chris123hoen@gmail.com>
2019-02-12 21:14:33 -0500

    more

--- Scratch/dodecagon.py
+++ Scratch/dodecagon.py
@@ -1,14 +1,26 @@
 from math import *
-
-def sind(a):
-   return sin(radians(a))
-
-def cosd(a): 
-   return cos(radians(a))
-   
-def tand(a): 
-   return tan(radians(a))
-   
-def atan2d(y,x): 
-   return degrees(atan2(y,x))
  
+ 
+center_row = 2160/2
+center_col = 3840/2
+ 
+r = 500
+a = 2*pi/24
+
+rows = []
+cols = [] 
+
+for n in range(0,12):
+   b = 2*pi*n/12 + a
+   rows.append(center_row + r*sin(b))
+   cols.append(center_col + r*cos(b))
+
+   
+print("floats")
+for n in range(0,12):
+   print("rc %10.2f %10.2f" % (rows[n],cols[n]))
+   
+print("ints")
+for n in range(0,12):
+   print("%5d %5d" % (round(rows[n]),round(cols[n])))
+   
