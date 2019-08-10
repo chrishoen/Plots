e806e82e0b7193797f823efa0831747d17ab2d07
chrishoen <chris123hoen@gmail.com>
2019-08-10 18:13:02 -0400

    more

--- Force/plot101.py
+++ Force/plot101.py
@@ -7,7 +7,6 @@ file_path = "./myforcelog.csv"
 
 def go1():
    t, x1, pwm, vstop = np.loadtxt(file_path, delimiter=",", unpack=True)
-   #data = np.loadtxt(file_path, delimiter=",")
 
    print("data.shape",x1.shape)
 
@@ -16,5 +15,28 @@ def go1():
    plt.xlabel('time')
    plt.ylabel('x1')
    plt.title('SignalHistory')
-   plt.legend()
+   plt.show()
+
+def go3():
+   t, x1, pwm, vstop = np.loadtxt(file_path, delimiter=",", unpack=True)
+
+   print("data.shape",x1.shape)
+
+   ax1 = plt.subplot(3,1,1)
+   plt.title('SignalHistory')
+   plt.plot(t,x1)
+   plt.ylabel('Force')
+   ax1.set_ylim([-5,5])
+
+   ax2 = plt.subplot(3,1,2)
+   plt.plot(t,pwm)
+   plt.ylabel('PWM')
+   ax2.set_ylim([-200,1000])
+
+   ax3 = plt.subplot(3,1,3)
+   plt.plot(t,vstop)
+   plt.xlabel('time')
+   plt.ylabel('VSTOP')
+   ax3.set_ylim([-1,2])
+
    plt.show()
